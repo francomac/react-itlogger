ef06451a8c49d9ef7f365fd9af7bc595709a6ffc
franco.martinez <franco.martinez@gorillalogic.com>
2022-02-02 14:25:04 -0600

    add logItem with delete btn + logs components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
+
+import Logs from './components/logs/Logs';
+import Searchbar from './components/layout/Searchbar';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -7,14 +10,19 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
+
   useEffect(() => {
     //Init Materialize JS
     M.AutoInit();
   })
+
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <Searchbar />
+      <div className="container">
+        <Logs />
+      </div>  
+    </Fragment>
   );
 }
 
