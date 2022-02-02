da6c860742f78ee710872253df083e4ae83ce106
franco.martinez <franco.martinez@gorillalogic.com>
2022-02-02 14:32:43 -0600

    add new button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {Fragment, useEffect} from 'react';
 
 import Logs from './components/logs/Logs';
 import Searchbar from './components/layout/Searchbar';
+import AddBtn from './components/layout/AddBtn';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -20,6 +21,7 @@ const App = () => {
     <Fragment>
       <Searchbar />
       <div className="container">
+        <AddBtn />
         <Logs />
       </div>  
     </Fragment>
